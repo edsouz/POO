30a34dae37949536faa8d385fde44088a0b651ec
edson <edsouz@gmail.com>
2018-06-29 10:50:49 -0300

    POO

---
+++ POO.py
@@ -0,0 +1,12 @@
+class Carro:    # Classe carro criada vazia
+    pass
+
+meu_carro = Carro() # Instanciando objeto meu_carro
+
+# Atributos do objeto meu_carro
+
+meu_carro.ano = 1968
+meu_carro.modelo = 'Fusca'
+meu_carro.cor = 'azul'
+
+print(meu_carro.cor)
